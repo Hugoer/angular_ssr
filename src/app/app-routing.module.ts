f9657d0c3220a2dec0e4117162ae87287ea67d06
DESKTOP-67C200L\Hugo <kuky85@gmail.com>
2019-03-28 14:22:08 +0100

    - Crear componentes y enrutarlos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { ListComponent } from './list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'profile',
+    component: ProfileComponent,
+  },
+  {
+    path: 'list',
+    component: ListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
